85b4957bdf99ecd1540bc35057b19937409e292f
skemil <skemil@outlook.com>
2023-11-12 21:37:06 +0100

    Update

--- myproject/models.py
+++ myproject/models.py
@@ -7,6 +7,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
+    name = Column(String, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
 
@@ -20,5 +21,5 @@ class Review(Base):
     stars = Column(Integer, index=True)
     description = Column(String, index=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
-
+    name = Column(String, ForeignKey("users.name"))
     owner = relationship("User", back_populates="reviews")
